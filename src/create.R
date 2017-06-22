a4e94582344024611847de8a56d2a3f3395e6b70
nacnudus <nacnudus@gmail.com>
2017-06-22 12:35:08 +0100

    Populate the organisation field with CURIEs

--- src/create.R
+++ src/create.R
@@ -15,28 +15,26 @@ codes <- read_csv(list_path,
                   na = c("", "n/a"))
 
 # Choose the fields for the register
-codes <- select(codes, 
+codes <- select(codes,
                 `Entity code`,
                 `Entity name`,
                 `Entity coverage`,
+                `Entity owner`,
                 `Date entity introduced on RGC`)
 
 # Give the fields register-like names
 colnames(codes) <- c("statistical-geography",
                      "name",
-                     "area-link",
+                     "area_link",
+                     "org_link",
                      "start-date")
-
-# Create an 'organisation' column.
-# For now, this must be manually populated.
-codes$organisation <- NA
 
 # Create an end-date column.
 # TODO: It should be the date that a code was archived
 codes$`end-date` <- NA
 
 # Map area to country/territory/uk registers
-mapper <- frame_data(~`area-link`,                     ~`area`,
+area_map <- frame_data(~area_link,                       ~area,
                         "England",                          NA,
                           "Wales",                          NA,
                        "Scotland",                          NA,
@@ -47,18 +45,86 @@ mapper <- frame_data(~`area-link`,                     ~`area`,
                 "Channel Islands", "territory:JE;territory:GG",
                     "Isle of Man",              "territory:IM")
 
-codes <- 
+# Map organisation to government-organisation register
+
+# Get the government-organisation register and filter for latest entries
+organisations <-
+  read_tsv("https://government-organisation.register.gov.uk/records.tsv?page-size=5000") %>%
+  group_by(key) %>%
+  arrange(key, `entry-timestamp`) %>%
+  filter(`entry-timestamp` == max(`entry-timestamp`)) %>%
+  ungroup() %>%
+  rename(organisation = "government-organisation") %>%
+  select(organisation, name)
+
+# org_map <-
+#   codes %>%
+#   distinct(org_link, area_link) %>%
+#   mutate(organisation = "government-organisation:") %>%
+#   select(org_link, area_link, organisation) %>%
+#   arrange(org_link, area_link) %>%
+#   deparse(as_tribble = TRUE)
+# # Output this line to the console and paste to base new tribble on old.
+# cat(org_map)
+
+org_map <- tribble(~org_link, ~area_link, ~organisation,
+                   "BCNI", "Northern Ireland", "government-organisation:OT428",
+                   "BIS", "England", "government-organisation:D3",
+                   "DCLG", "England", "government-organisation:D4",
+                   "DCLG", "Wales", "government-organisation:D4",
+                   "DEFRA", "England", "government-organisation:D7",
+                   "GLA", "England", "local-authority-eng:GLA",
+                   "HIE", "Scotland", NA,
+                   "Home Office", "England", "government-organisation:D16",
+                   "Home Office", "Wales", "government-organisation:D16",
+                   "LGBC", "England", NA,
+                   "LGBC", "Scotland", NA,
+                   "Natural England", "England", "government-organisation:PB202",
+                   "NHS", "England", "government-organisation:PB481",
+                   "NHS", "Scotland", NA,
+                   "NIFRS", "Northern Ireland", NA,
+                   "NISRA", "Northern Ireland", "government-organisation:EA701",
+                   "NRS", "Scotland", "government-organisation:OT1075",
+                   "ODS", "Channel Islands", "government-organisation:PB1202",
+                   "ODS", "England", "government-organisation:PB1202",
+                   "ODS", "Isle of Man", "government-organisation:PB1202",
+                   "ODS", "Wales", "government-organisation:PB1202",
+                   "ONS", "Channel Islands", "government-organisation:D303",
+                   "ONS", "England", "government-organisation:D303",
+                   "ONS", "England and Wales", "government-organisation:D303",
+                   "ONS", "Great Britain", "government-organisation:D303",
+                   "ONS", "Isle of Man", "government-organisation:D303",
+                   "ONS", "Northern Ireland", "government-organisation:D303",
+                   "ONS", "Scotland", "government-organisation:D303",
+                   "ONS", "United Kingdom", "government-organisation:D303",
+                   "ONS", "Wales", "government-organisation:D303",
+                   "PHE", "England", "government-organisation:EA480",
+                   "PSNI", "Northern Ireland", "government-organisation:OT657",
+                   "SE", "Scotland", NA,
+                   "SG", "Scotland", "government-organisation:DA1020",
+                   "WG", "Wales", "government-organisation:DA1019",
+                   "WG (BC)", "Wales", "government-organisation:OT432",
+                   "WG (Data Unit)", "Wales", NA,
+                   "WG (Econ)", "Wales", NA,
+                   "WG (Educ)", "Wales", NA,
+                   "WG (Envt)", "Wales", NA,
+                   "WG (Health)", "Wales", NA,
+                   "WG (LGBC)", "Wales", NA,
+                   "WG (Stats)", "Wales", NA)
+
+codes <-
   codes %>%
-  left_join(mapper, by = "area-link") %>%
+  left_join(area_map, by = "area_link") %>%
+  left_join(org_map, by = "org_link") %>%
   select(`statistical-geography`,
          name,
-         `area`,
-         `organisation`,
+         area,
+         organisation,
          `start-date`,
          `end-date`)
 
 # Tweak the formats
-codes <- 
+codes <-
   mutate(codes,
          # Use ISO8601 dates
          `start-date` =
